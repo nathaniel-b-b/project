59d373cc2eac8824fbb194bbd0a570a3adc2a44e
Nathaniel Buckley <nathbuck@s48.uppmax.uu.se>
2021-04-14 15:03:24 +0200

    Assembly files for batch jobs

---
+++ assembly_two.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+
+#SBATCH -A g2021012
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 1
+#SBATCH -t 18:00:00
+#SBATCH -J assemblyofmeta
+
+module load bioinfo-tools
+module load megahit
+
+srun megahit -1 Thrash/raw/SRR4342133_1.paired.trimmed.fastq.gz -2 Thrash/raw/SRR4342133_2.paired.trimmed.fastq.gz --kmin-1pass -o ass_of_meta_two
