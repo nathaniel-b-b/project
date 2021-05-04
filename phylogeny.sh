ab05094daf78658e9cc486d00c6bac14690d6ffc
nathaniel-b-b <81922755+nathaniel-b-b@users.noreply.github.com>
2021-05-04 16:52:41 +0200

    Update phylogeny.sh

--- phylogeny.sh
+++ phylogeny.sh
@@ -7,25 +7,28 @@
 #SBATCH -t 08:00:00
 #SBATCH -J phylo
 
+module purge
+
 module load bioinfo-tools
 module load phylophlan
-module load python/2.7.15
+module load biopython
 module load FastTree
 module load muscle
-module load usearch/5.3.32
+module load usearch
+module list
 
-ln -s /sw/apps/bioinfo/phylophlan/0.99/snowy/bin/data/*.tar.bz2 ~/Thrash/phylogeny/data
-ln -s /sw/apps/bioinfo/phylophlan/0.99/snowy/bin/data/*.txt ~/Thrash/phylogeny/data
-ln -s /sw/apps/bioinfo/phylophlan/0.99/snowy/bin/data/ppaalns/ppa.aln.tar.bz2 ~/Thrash/phylogeny/data/ppaalns
-ln -s /sw/apps/bioinfo/phylophlan/0.99/snowy/bin/taxcuration/* ~/Thrash/phylogeny/taxcuration
-ln -s /sw/apps/bioinfo/phylophlan/0.99/snowy/bin/phylophlan.py ~/Thrash/phylogeny
+ln -sf /sw/apps/bioinfo/phylophlan/0.99/snowy/bin/data/*.tar.bz2 ~/Thrash/phylogeny/data
+ln -sf /sw/apps/bioinfo/phylophlan/0.99/snowy/bin/data/*.txt ~/Thrash/phylogeny/data
+ln -sf /sw/apps/bioinfo/phylophlan/0.99/snowy/bin/data/ppaalns/ppa.aln.tar.bz2 ~/Thrash/phylogeny/data/ppaalns
+ln -sf /sw/apps/bioinfo/phylophlan/0.99/snowy/bin/taxcuration/* ~/Thrash/phylogeny/taxcuration
+ln -sf /sw/apps/bioinfo/phylophlan/0.99/snowy/bin/phylophlan.py ~/Thrash/phylogeny
 
 cd ~/Thrash/prokk
 
 for bins in *.fa
 do
-ln -s $bins/*.faa ~/Thrash/phylogeny/input/metagenome
+ln -sf $bins/*.faa ~/Thrash/phylogeny/input/metagenome
 done
 
 cd ~/Thrash/phylogeny
-python phylophlan.py -i metagenome -d phylophlan
+python phylophlan.py -i metagenome -d phylophlan --diversity medium
