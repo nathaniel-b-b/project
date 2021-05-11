7be5935dff7f00193579f4fbd9da7a07b9f3e6b7
nathaniel-b-b <81922755+nathaniel-b-b@users.noreply.github.com>
2021-05-11 16:32:09 +0200

    Update phylogeny.sh

--- phylogeny.sh
+++ phylogeny.sh
@@ -10,25 +10,25 @@
 module purge
 
 module load bioinfo-tools
+module load python/2.7.11
 module load phylophlan
-module load biopython
 module load FastTree
 module load muscle
-module load usearch
+module load usearch/5.2.32
 module list
+
 
 ln -sf /sw/apps/bioinfo/phylophlan/0.99/snowy/bin/data/*.tar.bz2 ~/Thrash/phylogeny/data
 ln -sf /sw/apps/bioinfo/phylophlan/0.99/snowy/bin/data/*.txt ~/Thrash/phylogeny/data
 ln -sf /sw/apps/bioinfo/phylophlan/0.99/snowy/bin/data/ppaalns/ppa.aln.tar.bz2 ~/Thrash/phylogeny/data/ppaalns
-ln -sf /sw/apps/bioinfo/phylophlan/0.99/snowy/bin/taxcuration/* ~/Thrash/phylogeny/taxcuration
 ln -sf /sw/apps/bioinfo/phylophlan/0.99/snowy/bin/phylophlan.py ~/Thrash/phylogeny
 
 cd ~/Thrash/prokk
 
 for bins in *.fa
 do
-ln -sf $bins/*.faa ~/Thrash/phylogeny/input/metagenome
+cp $bins/*.faa ~/Thrash/phylogeny/input/metagenome
 done
 
 cd ~/Thrash/phylogeny
-python phylophlan.py -i metagenome -d phylophlan --diversity medium
+phylophlan.py -i metagenome
