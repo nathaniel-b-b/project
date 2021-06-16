fb77da326100f029dc9b550df618d53cabd30337
nathaniel-b-b <81922755+nathaniel-b-b@users.noreply.github.com>
2021-06-16 22:27:43 +0200

    Create indexing.sh

---
+++ indexing.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+
+#SBATCH -A g2021012
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 1
+#SBATCH -t 1:00:00
+#SBATCH -J indexing
+
+cd ~/Thrash
+
+module load bioinfo-tools
+module load bwa
+
+for bins in binning/bins_1/*.fa
+bwa index binning/bins_1/$bins
