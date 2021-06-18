957b6c48c7a23c8b071c9ce4d5616ef91bd675e8
nathaniel-b-b <81922755+nathaniel-b-b@users.noreply.github.com>
2021-06-18 05:21:03 +0200

    Create htseqtwo.sh

---
+++ htseqtwo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+
+#SBATCH -A g2021012
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 8:00:00
+#SBATCH -J htseq
+
+module load bioinfo-tools
+module load htseq/0.9.1
+
+for bins in binning/bins_2/*.fa
+do
+htseq-count -f bam -r pos -t CDS -i ID -s no ~/Thrash/maps2/$bins.sorted.bam <(sed '/##FASTA/,$d' ~/Thrash/prokktwo/$bins/$bins.gff) > ~/Thrash/countstoo/$bins.readcount.out
+done
