66ebbe872a452bfa43f76ace800dab905ddfd60b
nathaniel-b-b <81922755+nathaniel-b-b@users.noreply.github.com>
2021-06-18 05:18:28 +0200

    Update htseq.sh

--- htseq.sh
+++ htseq.sh
@@ -5,7 +5,12 @@
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -t 8:00:00
-#SBATCH -J Htseq
+#SBATCH -J htseq
 
 module load bioinfo-tools
 module load htseq/0.9.1
+
+for bins in binning/bins_1/*.fa
+do
+htseq-count -f bam -r pos -t CDS -i ID -s no ~/Thrash/maps/$bins.sorted.bam <(sed '/##FASTA/,$d' ~/Thrash/prokk/$bins/$bins.gff) > ~/Thrash/counts/$bins.readcount.out
+done
